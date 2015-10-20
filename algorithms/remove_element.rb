609c5c5bf1839668116ea9284452cb957f437ed7
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:55:59 +0800

    solve: Remove Element

---
+++ algorithms/remove_element.rb
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/remove-element/
+#
+# Given an array and a value, remove all instances of that value in
+# place and return the new length. The order of elements can be changed.
+# It doesn't matter what you leave beyond the new length.
+
+
+# @param {Integer[]} nums
+# @param {Integer} val
+# @return {Integer}
+def remove_element(nums, val)
+  nums.delete(val)
+  nums.size
+end
