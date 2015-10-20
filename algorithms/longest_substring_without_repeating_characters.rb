e7a32bcece4aa3e277c32f721eb3947243fe4c95
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:26:30 +0800

    solve: Longest Substring Without Repeating Characters

---
+++ algorithms/longest_substring_without_repeating_characters.rb
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+#
+# Given a string, find the length of the longest substring without
+# repeating characters. For example, the longest substring without
+# repeating letters for "abcabcbb" is "abc", which the length is 3.
+# For "bbbbb" the longest substring is "b", with the length of 1.
+
+
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+  substr, maxlen = '', 0
+
+  s.each_char do |ch|
+    if substr.include?(ch)
+      maxlen = substr.length if maxlen < substr.length
+      substr = substr[(substr.index(ch) + 1)..-1]
+    end
+    substr << ch
+  end
+  maxlen = substr.length if maxlen < substr.length
+
+  maxlen
+end
