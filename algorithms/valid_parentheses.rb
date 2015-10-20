de62a3fc4ba092e2fe6682bb56609cf1a53ee0c1
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:34:14 +0800

    solve: Valid Parentheses

---
+++ algorithms/valid_parentheses.rb
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/valid-parentheses/
+#
+# Given a string containing just the characters '(', ')', '{', '}',
+# '[' and ']', determine if the input string is valid. The brackets
+# must close in the correct order, "()" and "()[]{}" are all valid
+# but "(]" and "([)]" are not.
+
+
+# @param {String} s
+# @return {Boolean}
+def is_valid(s)
+  stack = []
+
+  s.each_char do |ch|
+    case ch
+    when '(', '{', '['
+      stack.push(ch)
+    when ')'
+      return false if stack.pop != '('
+    when '}'
+      return false if stack.pop != '{'
+    when ']'
+      return false if stack.pop != '['
+    else
+      nil
+    end
+  end
+
+  stack.empty?
+end
