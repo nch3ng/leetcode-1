6b3db45b23e7941ddc5f75dbc1ad3b475f0fa8ee
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:48:20 +0800

    solve: House Robber I && II

---
+++ algorithms/house_robber.rb
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/house-robber/
+#
+# You are a professional robber planning to rob houses along a street. Each
+# house has a certain amount of money stashed, the only constraint stopping
+# you from robbing each of them is that adjacent houses have security
+# system connected and it will automatically contact the police if
+# two adjacent houses were broken into on the same night.
+#
+# Given a list of non-negative integers representing the amount of money
+# of each house, determine the maximum amount of money you can rob
+# tonight without alerting the police.
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def rob(nums)
+  rob_include_nth, rob_exclude_nth = 0, 0
+
+  nums.each do |num|
+    rob_include_nth, rob_exclude_nth =
+      rob_exclude_nth + num, [rob_exclude_nth, rob_include_nth].max
+  end
+
+  return rob_include_nth if rob_include_nth > rob_exclude_nth
+  return rob_exclude_nth
+end
---
+++ algorithms/house_robber_ii.rb
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/house-robber-ii/
+#
+# This is an extension of "House Robber".
+#
+# After robbing those houses on that street, the thief has found himself
+# a new place for his thievery so that he will not get too much attention.
+# This time, all houses at this place are arranged in a circle. That means
+# the first house is the neighbor of the last one. Meanwhile, the security
+# system for these houses remain the same as for those in the previous street.
+#
+# Given a list of non-negative integers representing the amount of money
+# of each house, determine the maximum amount of money you can rob tonight
+# without alerting the police.
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def rob(nums)
+  return nums[0] if nums.size == 1
+  return [_rob_(nums, 0, nums.size - 2), _rob_(nums, 1, nums.size - 1)].max
+end
+
+private def _rob_(nums, lbound, ubound)
+  rob_include_nth, rob_exclude_nth = 0, 0
+
+  lbound.upto(ubound).each do |idx|
+    rob_include_nth, rob_exclude_nth =
+      rob_exclude_nth + nums[idx], [rob_exclude_nth, rob_include_nth].max
+  end
+
+  return rob_include_nth if rob_include_nth > rob_exclude_nth
+  return rob_exclude_nth
+end
