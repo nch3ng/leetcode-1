a98e9a3e24801ebebee5a16059d45a23b49dc1fc
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:41:02 +0800

    solve: Missing Number

---
+++ algorithms/missing_number.rb
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/missing-number/
+#
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n,
+# find the one that is missing from the array.
+#
+# For example,
+#
+#     Given nums = [0, 1, 3] return 2.
+#
+# Note: Your algorithm should run in linear runtime complexity. Could you
+# implement it using only constant extra space complexity?
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def missing_number(nums)
+  nums.size * (nums.size + 1) / 2 - nums.reduce(&:+)
+end
