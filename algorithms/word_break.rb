67beec6524cc29bcba991732d97fd8559022955d
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:36:46 +0800

    solve: Word Break I && II

---
+++ algorithms/word_break.rb
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/word-break/
+#
+# Given a string s and a dictionary of words dict, determine if s can
+# be segmented into a space-separated sequence of one or more dictionary
+# words.
+#
+# For example,
+#
+#     given s = "leetcode", dict = ["leet", "code"].
+#     Return true because "leetcode" can be segmented as "leet code".
+
+
+# @param {String} s
+# @param {Set<String>} word_dict
+# @return {Boolean}
+def word_break(s, word_dict)
+  _word_break_(s, 0, s.size - 1, word_dict, Array.new(s.size))
+end
+
+private def _word_break_(s, lbound, ubound, word_dict, marks)
+  return true if lbound > ubound
+
+  lbound.upto(ubound).each do |spt|
+    next if  marks[spt]
+    next if !word_dict.include?(s[lbound..spt])
+
+    marks[spt] = true
+    return true if _word_break_(s, spt + 1, ubound, word_dict, marks)
+  end
+
+  return false
+end
---
+++ algorithms/word_break_ii.rb
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/word-break-ii/
+#
+# Given a string s and a dictionary of words dict, add spaces in s
+# to construct a sentence where each word is a valid dictionary
+# word. Return all such possible sentences.
+#
+# For example,
+#
+#     given s = "catsanddog", dict = ["cat", "cats", "and", "sand", "dog"].
+#     A solution is ["cats and dog", "cat sand dog"].
+
+
+# @param {String} s
+# @param {Set<String>} word_dict
+# @return {String[]}
+def word_break(s, word_dict)
+  [].tap do |result|
+    _word_break_(s, 0, s.size - 1, word_dict, Array.new(s.size), [0], result)
+  end
+end
+
+private def _word_break_(s, lbound, ubound, word_dict, marks, tracing, result)
+  if lbound > ubound
+    result << tracing.each_cons(2).map(&->(l, u){ s[l...u] }).join(' ')
+    return true
+  end
+
+  ok = false
+  lbound.upto(ubound).each do |spt|
+    next if  marks[spt]
+    next if !word_dict.include?(s[lbound..spt])
+
+    tracing.push(spt + 1)
+    if _word_break_(s, spt + 1, ubound, word_dict, marks, tracing, result)
+      ok = true
+    end
+    tracing.pop
+  end
+
+   marks[lbound - 1] = true unless ok
+   return ok
+end
