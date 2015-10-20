f55c58847640f79c5d61dd9dd74b0cfa3680045a
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:18:23 +0800

    solve: Evaluate Reverse Polish Notation

---
+++ algorithms/evaluate_reverse_polish_notation.rb
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/
+#
+# Evaluate the value of an arithmetic expression in Reverse Polish
+# Notation. Valid operators are +, -, *, /. Each operand may be
+# an integer or another expression.
+#
+# Some examples:
+#
+#     ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+#     ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+
+
+# @param {String[]} tokens
+# @return {Integer}
+def eval_rpn(tokens)
+  s = []
+
+  tokens.each do |token|
+    case token
+    when '+'; s.push(s.pop + s.pop)
+    when '-'; v1, v2 = s.pop, s.pop; s.push(v2 - v1)
+    when '*'; s.push(s.pop * s.pop)
+    when '/'; v1, v2 = s.pop, s.pop; s.push(v2.fdiv(v1).truncate)
+    else    ; s.push(token.to_i)
+    end
+  end
+
+  s.pop
+end
