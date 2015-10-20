4d11e33c38426b7105b6ae681aab5f07f675040e
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 16:47:04 +0800

    solve: Binary Search Tree Iterator

---
+++ algorithms/binary_search_tree_iterator.rb
@@ -0,0 +1,79 @@
+# https://leetcode.com/problems/binary-search-tree-iterator/
+#
+# Implement an iterator over a binary search tree (BST). Your iterator
+# will be initialized with the root node of a BST.
+#
+# Calling next() will return the next smallest number in the BST.
+#
+# Note: next() and hasNext() should run in average O(1) time and uses
+# O(h) memory, where h is the height of the tree.
+
+
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
+# end
+
+
+class BSTIterator
+  # @param {TreeNode} root
+  def initialize(root)
+    @fiber = Fiber.new { _inorder_traversal_(root) }
+    @node, @nextnode = nil, @fiber.resume
+  end
+
+  # @return {Boolean}
+  def has_next
+    !!@nextnode
+  end
+
+  # @return {Integer}
+  def next
+    @node, @nextnode = @nextnode, @fiber.resume
+    @node.val
+  end
+
+  # @return {TreeNode}
+  private def _inorder_traversal_(root)
+    return nil if root.nil?
+
+    node, stack = root, []
+    while true
+      Fiber.yield node if node.left.nil?
+
+      if node.left || node.right
+        stack.push(node)
+        node = node.left || node.right
+        next
+      end
+
+      while true
+        pnode = stack.pop
+        return nil if pnode.nil?
+
+        if node == pnode.left
+          Fiber.yield pnode
+          node = pnode
+          next if node.right.nil?
+          stack.push(node)
+          node = node.right
+          break
+        else
+          node = pnode
+          next
+        end
+      end
+    end
+  end
+end
+
+
+# Your BSTIterator will be called like this:
+# i, v = BSTIterator.new(root), []
+# while i.has_next()
+#   v << i.next
+# end
