931d805eb95ecf17c9e84ede8a9d625844057027
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:25:53 +0800

    solve: Summary Ranges

---
+++ algorithms/summary_ranges.rb
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/summary-ranges/
+#
+# Given a sorted integer array without duplicates, return the summary of
+# its ranges.
+#
+# For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+
+
+# @param {Integer[]} nums
+# @return {String[]}
+def summary_ranges(nums)
+  cursor, ranges = 0, []
+
+  while cursor < nums.size
+    reference = cursor
+    cursor += 1 while nums[cursor] + 1 == nums[cursor + 1]
+
+    if cursor == reference
+      ranges << "#{nums[reference]}"
+    else
+      ranges << "#{nums[reference]}->#{nums[cursor]}"
+    end
+
+    cursor += 1
+  end
+
+  ranges
+end
