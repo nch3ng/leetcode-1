c46b7a734bcb91b8cc61c77d9117fb25acabb799
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:37:15 +0800

    solve: Min Stack

---
+++ algorithms/min_stack.rb
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/min-stack/
+#
+# Design a stack that supports push, pop, top, and retrieving the
+# minimum element in constant time.
+#
+#     push(x) -- Push element x onto stack.
+#     pop() -- Removes the element on top of the stack.
+#     top() -- Get the top element.
+#     getMin() -- Retrieve the minimum element in the stack.
+
+
+class MinStack
+  # Initialize your data structure here
+  def initialize
+    @stack = []
+    @min_stack = []
+  end
+
+  # @param {Integer} x
+  # @return {Void} nothing
+  def push(x)
+    @stack.push(x)
+    @min_stack.push(x) if @min_stack.empty? || @min_stack[-1] >= x
+  end
+
+  # @return {Void} nothing
+  def pop
+    @min_stack.pop if @min_stack[-1] == @stack[-1]
+    @stack.pop; nil
+  end
+
+  # @return {Integer}
+  def top
+    @stack[-1]
+  end
+
+  # @return {Integer}
+  def get_min
+    @min_stack[-1]
+  end
+end
