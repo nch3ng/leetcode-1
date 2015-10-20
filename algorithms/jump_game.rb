da7c85140ee550a544b66021b871f450794eb1c0
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:11:01 +0800

    solve: Jump Game I && II

---
+++ algorithms/jump_game.rb
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/jump-game/
+#
+# Given an array of non-negative integers, you are initially positioned
+# at the first index of the array. Each element in the array represents
+# your maximum jump length at that position. Determine if you are able
+# to reach the last index.
+#
+# For example:
+#
+#     A = [2,3,1,1,4], return true.
+#     A = [3,2,1,0,4], return false.
+
+
+# @param {Integer[]} nums
+# @return {Boolean}
+def can_jump(nums)
+  max, submax = 0, 0
+
+  lbound, ubound = 0, nums.size - 1
+  while lbound <= ubound && lbound <= max
+    submax = lbound + nums[lbound]
+    max = submax if max <  submax
+    return true  if max >= ubound
+
+    lbound += 1
+  end
+
+  max >= ubound
+end
---
+++ algorithms/jump_game_ii.rb
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/jump-game-ii/
+#
+# Given an array of non-negative integers, you are initially positioned
+# at the first index of the array. Each element in the array represents
+# your maximum jump length at that position. Your goal is to reach the
+# last index in the minimum number of jumps.
+#
+# For example:
+#
+#     Given array A = [2,3,1,1,4]
+#     The minimum number of jumps to reach the last index is 2. (Jump 1
+#     step from index 0 to 1, then 3 steps to the last index.)
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def jump(nums)
+  max, submax, steps = 0, 0, 1
+
+  lbound, ubound, maxbound = 0, nums.size - 1, max
+  return 0 if max >= ubound
+
+  while true
+    lbound.upto(maxbound) do |idx|
+      submax = idx + nums[idx]
+      max = submax if max <  submax
+      return steps if max >= ubound
+    end
+
+    steps += 1
+    lbound, maxbound = maxbound + 1, max
+  end
+end
