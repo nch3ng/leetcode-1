d829f281be2f907f48fb2dd85a1baf4107833d73
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:32:21 +0800

    solve: Maximum Product Subarray

---
+++ algorithms/maximum_product_subarray.rb
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/maximum-product-subarray/
+#
+# Find the contiguous subarray within an array (containing at least one
+# number) which has the largest product.
+#
+# For example, given the array [2,3,-2,4], the contiguous subarray [2,3]
+# has the largest product = 6.
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def max_product(nums)
+  product, lsubproduct, rsubproduct = [nums[0], nums[-1]].max, nums[0], nums[-1]
+
+  (1...nums.size).each do |idx|
+    lsubproduct  = 1 if lsubproduct == 0
+    lsubproduct *= nums[idx]
+    product = lsubproduct if product < lsubproduct
+
+    rsubproduct  = 1 if rsubproduct == 0
+    rsubproduct *= nums[-1 + -idx]
+    product = rsubproduct if product < rsubproduct
+  end
+
+  product
+end
