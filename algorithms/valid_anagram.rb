00b267211b01e4beca89f60e322344f2ddd4f541
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:32:50 +0800

    solve: Valid Anagram

---
+++ algorithms/valid_anagram.rb
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/valid-anagram/
+#
+# Given two strings s and t, write a function to determine if t is
+# an anagram of s.
+#
+# For example,
+#
+#     s = "anagram", t = "nagaram", return true.
+#     s = "rat", t = "car", return false.
+#
+# Note: You may assume the string contains only lowercase alphabets.
+
+
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_anagram(s, t)
+  return false if s.size != t.size
+
+  m = Range.new('a', 'z').each_with_object({}) { |ch, memo| memo[ch] = 0 }
+  s.each_char { |ch| m[ch] += 1 }
+  t.each_char { |ch| m[ch] -= 1 }
+  m.values.all?(&:zero?)
+end
