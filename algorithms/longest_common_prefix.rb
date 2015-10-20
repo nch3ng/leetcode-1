257dc10636d17270bd79f54ea5f86d1a7c076685
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:15:23 +0800

    solve: Longest Common Prefix

---
+++ algorithms/longest_common_prefix.rb
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/longest-common-prefix/
+#
+# Write a function to find the longest common prefix string amongst
+# an array of strings.
+
+
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+  return '' if strs.empty?
+
+  str, cnt = strs[0], 0
+  str.chars.each_with_index do |ch, idx|
+    break if strs[1..-1].any? { |s| s[idx] != ch }
+    cnt += 1
+  end
+  str.slice(0, cnt)
+end
