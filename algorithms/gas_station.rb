8dabb20d0f05a4d82ba9e2d4d3a90c2b866f6b59
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:44:33 +0800

    solve: Gas Station

---
+++ algorithms/gas_station.rb
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/gas-station/
+#
+# There are N gas stations along a circular route, where the amount of gas
+# at station i is gas[i]. You have a car with an unlimited gas tank and it
+# costs cost[i] of gas to travel from station i to its next station (i+1).
+# You begin the journey with an empty tank at one of the gas stations.
+#
+# Return the starting gas station's index if you can travel around the
+# circuit once, otherwise return -1.
+#
+# Note: The solution is guaranteed to be unique.
+
+
+# @param {Integer[]} gas
+# @param {Integer[]} cost
+# @return {Integer}
+def can_complete_circuit(gas, cost)
+  n = gas.size
+  sum, lbound, ubound = gas[0], -1, 0
+
+  while ubound - lbound != n
+    if sum - cost[ubound] >= 0
+      sum += - cost[ubound] + gas[ubound + 1]
+      ubound += 1
+    else
+      sum += - cost[lbound] + gas[lbound]
+      lbound -= 1
+    end
+  end
+  sum += - cost[lbound]
+
+  sum < 0 ? -1 : (lbound + 1 + n) % n
+end
