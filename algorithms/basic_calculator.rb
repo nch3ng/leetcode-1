0bd499206b9e433e835eca2666afab720d1aef7e
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 16:21:08 +0800

    solve: Basic Calculator I && II

---
+++ algorithms/basic_calculator.rb
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/basic-calculator/
+#
+# Implement a basic calculator to evaluate a simple expression string.
+# The expression string may contain open ( and closing parentheses ),
+# the plus + or minus sign -, non-negative integers and empty spaces.
+# You may assume that the given expression is always valid.
+#
+# Some examples:
+#
+#     "1 + 1" = 2
+#     " 2-1 + 2 " = 3
+#     "(1+(4+5+2)-3)+(6+8)" = 23
+#
+# Note: Do not use the eval built-in library function.
+
+
+# @param {String} s
+# @return {Integer}
+def calculate(s)
+  s, s1, s2 = "(#{s})", [], []
+
+  s.scan(/ \s | \( | \) | [+-] | \d+ /x) do |token|
+    case token
+    when ' '
+      next
+    when '('
+      s1.push('(')
+    when ')'
+      case s1.pop
+      when '('; next
+      when '+'; s2.push(s2.pop + s2.pop); s1.pop
+      when '-'; s2.push(- s2.pop + s2.pop); s1.pop
+      end
+    when '+', '-'
+      case s1.pop
+      when nil; s1.push(token)
+      when '('; s1.push('('); s1.push(token)
+      when '+'; s2.push(s2.pop + s2.pop); s1.push(token)
+      when '-'; s2.push(- s2.pop + s2.pop); s1.push(token)
+      end
+    else
+      s2.push(token.to_i)
+    end
+  end
+
+  s2.pop
+end
---
+++ algorithms/basic_calculator_ii.rb
@@ -0,0 +1,52 @@
+# https://leetcode.com/problems/basic-calculator-ii/
+#
+# Implement a basic calculator to evaluate a simple expression string.
+# The expression string contains only non-negative integers, +, -, *,
+# / operators and empty spaces. The integer division should truncate
+# toward zero. You may assume that the given expression is always valid.
+#
+# Some examples:
+#
+#     "3+2*2" = 7
+#     " 3/2 " = 1
+#     " 3+5 / 2 " = 5
+#
+# Note: Do not use the eval built-in library function.
+
+
+# @param {String} s
+# @return {Integer}
+def calculate(s)
+  s, s1, s2 = "#{s}+0", [], []
+
+  s.scan(/ \s | [+-] | [\*\/] | \d+ /x) do |token|
+    case token
+    when ' '
+      next
+    when '+', '-'
+      while true
+        case s1.pop
+        when nil; break
+        when '+'; s2.push(s2.pop + s2.pop)
+        when '-'; v1, v2 = s2.pop, s2.pop; s2.push(v2 - v1)
+        when '*'; s2.push(s2.pop * s2.pop)
+        when '/'; v1, v2 = s2.pop, s2.pop; s2.push(v2.fdiv(v1).truncate)
+        end
+      end
+      s1.push(token)
+    when '*', '/'
+      case s1.pop
+      when '+'; s1.push('+')
+      when '-'; s1.push('-')
+      when '*'; s2.push(s2.pop * s2.pop)
+      when '/'; v1, v2 = s2.pop, s2.pop; s2.push(v2.fdiv(v1).truncate)
+      end
+      s1.push(token)
+    else
+      s2.push(token.to_i)
+    end
+  end
+
+  s2.pop
+  s2.pop
+end
