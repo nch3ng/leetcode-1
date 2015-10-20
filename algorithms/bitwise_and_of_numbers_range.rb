55e62bd6b52c745a18f8b7ebc8b22261c3222c94
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 16:58:26 +0800

    solve: Bitwise AND of Numbers Range

---
+++ algorithms/bitwise_and_of_numbers_range.rb
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/bitwise-and-of-numbers-range/
+#
+# Given a range [m, n] where 0 <= m <= n <= 2147483647, return the
+# bitwise AND of all numbers in this range, inclusive.
+#
+# For example, given the range [5, 7], you should return 4.
+
+
+# @param {Integer} m
+# @param {Integer} n
+# @return {Integer}
+def range_bitwise_and(m, n)
+  c = 0
+  while m != n
+    m >>= 1
+    n >>= 1
+    c  += 1
+  end
+  m << c
+end
