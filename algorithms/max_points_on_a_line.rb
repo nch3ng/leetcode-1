1c391bb2d27a3b91a901cf12d446d832e3a06aad
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:30:09 +0800

    solve: Max Points on a Line

---
+++ algorithms/max_points_on_a_line.rb
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/max-points-on-a-line/
+#
+# Given n points on a 2D plane, find the maximum number of points that lie
+# on the same straight line.
+
+
+# Definition for a point.
+# class Point
+#   attr_accessor :x, :y
+#   def initialize(x=0, y=0)
+#     @x = x
+#     @y = y
+#   end
+# end
+
+
+# @param {Point[]} points
+# @return {Integer}
+def max_points(points)
+  max = 0
+
+  points.each_with_index do |p1, i1|
+    count, dupcount = {}, 0
+
+    points.each_with_index do |p2, i2|
+      if p1.x == p2.x && p1.y == p2.y
+        dupcount += 1; next
+      end
+
+      m = (p1.x == p2.x ? Float::INFINITY : (p1.y - p2.y).fdiv(p1.x - p2.x))
+      count[m] ||= 0
+      count[m]  += 1
+    end
+
+    submax = (count.values.max || 0) + dupcount
+    max = submax if max < submax
+  end
+
+  max
+end
