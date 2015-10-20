d825501ef093183b6aa34500fba2e1e1c6006f23
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:00:24 +0800

    solve: Reverse Bits

---
+++ algorithms/reverse_bits.rb
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/reverse-bits/
+#
+# Reverse bits of a given 32 bits unsigned integer.
+#
+# For example, given input 43261596 (represented in binary
+# as 00000010100101000001111010011100), return 964176192 (represented
+# in binary as 00111001011110000010100101000000).
+
+
+# @param {Integer} n, a positive integer
+# @return {Integer}
+def reverse_bits(n)
+  m = n & 1
+
+  31.times do |i|
+    n >>= 1
+    m <<= 1
+    m  += n & 1
+  end
+
+  m
+end
