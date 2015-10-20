f945afae79ae4a26ef74a30bf069b6e72e8456d4
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:07:02 +0800

    solve: Rotate Array

---
+++ algorithms/rotate_array.rb
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/rotate-array/
+#
+# Rotate an array of n elements to the right by k steps.
+#
+# For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7]
+# is rotated to [5,6,7,1,2,3,4].
+
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+  k.times { nums.unshift(nums.pop) }; nil
+end
