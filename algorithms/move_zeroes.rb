502107bc9c2aa6a4d109370be70e67ec18f097f9
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:41:16 +0800

    solve: Move Zeroes

---
+++ algorithms/move_zeroes.rb
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/move-zeroes/
+#
+# Given an array nums, write a function to move all 0's to the end of it
+# while maintaining the relative order of the non-zero elements.
+#
+# For example, given nums = [0, 1, 0, 3, 12], after calling your function,
+# nums should be [1, 3, 12, 0, 0].
+#
+# Note:
+#
+#     + You must do this in-place without making a copy of the array.
+#     + Minimize the total number of operations.
+
+
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
+def move_zeroes(nums)
+  idx = nums.index(0)
+  return if idx.nil?
+
+  cursor = idx
+  idx.upto(nums.size - 1) do |i|
+    if nums[i].nonzero?
+      nums[cursor] = nums[i]
+      cursor += 1
+    end
+  end
+
+  idx = cursor
+  idx.upto(nums.size - 1) do |i|
+    nums[i] = 0
+  end; nil
+end
