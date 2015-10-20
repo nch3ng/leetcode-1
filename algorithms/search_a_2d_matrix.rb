96ce1159ef6c11cf4f133a68d3295b05bdb2b531
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:08:55 +0800

    solve: Search a 2D Matrix

---
+++ algorithms/search_a_2d_matrix.rb
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/search-a-2d-matrix/
+#
+# Write an efficient algorithm that searches for a value in an m x n
+# matrix. This matrix has the following properties:
+#
+#     + Integers in each row are sorted from left to right.
+#     + The first integer of each row is greater than the last integer
+#       of the previous row.
+#
+# For example, Consider the following matrix:
+#
+#     [
+#       [1,   3,  5,  7],
+#       [10, 11, 16, 20],
+#       [23, 30, 34, 50]
+#     ]
+#
+# Given target = 3, return true.
+
+
+# @param {Integer[][]} matrix
+# @param {Integer} target
+# @return {Boolean}
+def search_matrix(matrix, target)
+  return false if matrix.empty?
+
+  m, n = matrix.size, matrix[0].size
+  mnum = Range.new(0, m * n - 1).bsearch do |ij|
+    i, j = ij.divmod(n); matrix[i][j] >= target
+  end || 0
+
+  i, j = mnum.divmod(n)
+  matrix[i][j] == target
+end
---
+++ algorithms/search_a_2d_matrix_ii.rb
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/search-a-2d-matrix-ii/
+#
+# Write an efficient algorithm that searches for a value in an m x n
+# matrix. This matrix has the following properties:
+#
+#     + Integers in each row are sorted in ascending from left to right.
+#     + Integers in each column are sorted in ascending from top to bottom.
+#
+# For example, Consider the following matrix:
+#
+#     [
+#       [1,   4,  7, 11, 15],
+#       [2,   5,  8, 12, 19],
+#       [3,   6,  9, 16, 22],
+#       [10, 13, 14, 17, 24],
+#       [18, 21, 23, 26, 30]
+#     ]
+#
+# Given target = 5, return true.
+# Given target = 20, return false.
+
+
+# @param {Integer[][]} matrix
+# @param {Integer} target
+# @return {Boolean}
+def search_matrix(matrix, target)
+  return false if matrix.empty?
+
+  m, n = matrix.size, matrix[0].size
+  i, j = 0, n - 1
+  while i != m && j != -1
+    case matrix[i][j] <=> target
+    when -1; i += 1
+    when  0; return true
+    when  1; j -= 1
+    end
+  end
+  return false
+end
