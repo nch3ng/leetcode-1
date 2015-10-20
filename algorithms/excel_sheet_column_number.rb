8ed5a3d92279515916896d7224eb88028e1eaae9
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:19:05 +0800

    solve: Excel Sheet Column Number

---
+++ algorithms/excel_sheet_column_number.rb
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/excel-sheet-column-number/
+#
+# Given a column title as appear in an Excel sheet, return its
+# corresponding column number.
+#
+# For example:
+#
+#     A -> 1
+#     B -> 2
+#     C -> 3
+#     ...
+#     Z -> 26
+#     AA -> 27
+#     AB -> 28
+
+
+# @param {String} s
+# @return {Integer}
+def title_to_number(s)
+  m = {}
+  Range.new('A', 'Z').each_with_index { |ch, idx| m[ch] = idx + 1 }
+
+  sum = 0
+  s.each_char { |ch| sum = sum * 26 + m[ch] }
+  sum
+end
