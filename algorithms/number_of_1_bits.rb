e749773bf84c7886eda6468900d976d5ad0e83d9
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:44:21 +0800

    solve: Number of 1 Bits

---
+++ algorithms/number_of_1_bits.rb
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/number-of-1-bits/
+#
+# Write a function that takes an unsigned integer
+# and returns the number of ’1' bits it has (also
+# known as the Hamming weight).
+#
+# For example, the 32-bit integer ’11' has binary
+# representation 00000000000000000000000000001011,
+# so the function should return 3.
+
+
+# @param {Integer} n, a positive integer
+# @return {Integer}
+def hamming_weight(n)
+  count = 0
+  while n.nonzero?
+    count += 1
+    n &= n - 1
+  end
+  count
+end
