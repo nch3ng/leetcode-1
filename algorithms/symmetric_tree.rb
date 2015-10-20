a606ce65a43050c8d5a82deec89cfa6317b10afb
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:27:38 +0800

    solve: Symmetric Tree

---
+++ algorithms/symmetric_tree.rb
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/symmetric-tree/
+#
+# Given a binary tree, check whether it is a mirror of
+# itself (ie, symmetric around its center).
+#
+# For example, this binary tree is symmetric:
+#
+#         1
+#        / \
+#       2   2
+#      / \ / \
+#     3  4 4  3
+#
+# But the following is not:
+#
+#         1
+#        / \
+#       2   2
+#      / \ / \
+#        3    3
+
+
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
+# end
+
+
+# @param {TreeNode} root
+# @return {Boolean}
+def is_symmetric(root)
+  return true if root.nil?
+  return _is_mirror_(root.left, root.right)
+end
+
+private def _is_mirror_(l, r)
+  return true  if l.nil? && r.nil?
+  return false if l.nil? || r.nil? || l.val != r.val
+  return _is_mirror_(l.left, r.right) && _is_mirror_(l.right, r.left)
+end
