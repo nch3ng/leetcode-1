3f9b00a015d75af7e2d785859efdec3e9e97e1c4
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:45:07 +0800

    solve: Number of Islands

---
+++ algorithms/number_of_islands.rb
@@ -0,0 +1,59 @@
+# https://leetcode.com/problems/number-of-islands/
+#
+# Given a 2d grid map of '1's (land) and '0's (water), count the number
+# of islands. An island is surrounded by water and is formed by connecting
+# adjacent lands horizontally or vertically. You may assume all four edges
+# of the grid are all surrounded by water.
+#
+# Example 1:
+#
+#     11110
+#     11010
+#     11000
+#     00000
+#
+#     Answer: 1
+#
+# Example 2:
+#
+#     11000
+#     11000
+#     00100
+#     00011
+#
+#     Answer: 3
+
+
+# @param {Character[][]} grid
+# @return {Integer}
+def num_islands(grid)
+  return 0 if grid.empty?
+
+  m, n = grid.size, grid[0].size
+  numb = 0
+  cmap = {}
+  0.upto(m * n - 1) do |ij|
+    next if cmap[ij]
+
+    i, j = ij.divmod(n)
+    next if grid[i][j] != '1'
+
+    q = [ij]
+    until q.empty?
+      coordinate = q.shift
+      next if cmap[coordinate]
+
+      x, y = coordinate.divmod(n)
+      q << coordinate - n if     x > 0 && grid[x - 1][y] == '1'
+      q << coordinate + n if x + 1 < m && grid[x + 1][y] == '1'
+      q << coordinate - 1 if     y > 0 && grid[x][y - 1] == '1'
+      q << coordinate + 1 if y + 1 < n && grid[x][y + 1] == '1'
+
+      cmap[coordinate] = true
+    end
+
+    numb += 1
+  end
+
+  numb
+end
