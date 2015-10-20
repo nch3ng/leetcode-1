cf58b3abf145fd60e90249c024c12517c2027d3e
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:21:58 +0800

    solve: Subset I && II

---
+++ algorithms/subsets.rb
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/subsets/
+#
+# Given a set of distinct integers, nums, return all possible subsets.
+#
+# Note:
+#
+#     + Elements in a subset must be in non-descending order.
+#     + The solution set must not contain duplicate subsets.
+#
+# For example, If nums = [1,2,3], a solution is:
+#
+#     [
+#       [3],
+#       [1],
+#       [2],
+#       [1,2,3],
+#       [1,3],
+#       [2,3],
+#       [1,2],
+#       []
+#     ]
+
+
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def subsets(nums)
+  nums.sort.each_with_object([[]]) do |num, memo|
+    memo.concat(memo.map(&->(s){ s.dup << num }))
+  end
+end
---
+++ algorithms/subsets_ii.rb
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/subsets-ii/
+#
+# Given a collection of integers that might contain duplicates, nums,
+# return all possible subsets.
+#
+# Note:
+#
+#     + Elements in a subset must be in non-descending order.
+#     + The solution set must not contain duplicate subsets.
+#
+# For example, If nums = [1,2,2], a solution is:
+#
+#     [
+#       [2],
+#       [1],
+#       [1,2,2],
+#       [2,2],
+#       [1,2],
+#       []
+#     ]
+
+
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def subsets_with_dup(nums)
+  nums.sort.each_with_object([[]]) do |num, memo|
+    memo.concat(memo.map(&->(s){ s.dup << num }))
+  end.uniq
+end
