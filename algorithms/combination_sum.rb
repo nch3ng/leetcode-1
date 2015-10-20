967692a9173bd75652db751f4a7ece41ca216568
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:02:23 +0800

    solve: Combination Sum I && II && III

---
+++ algorithms/combination_sum.rb
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/combination-sum/
+#
+# Given a set of candidate numbers(C) and a target number(T), find all
+# unique combinations in C where the candidate numbers sums to T.
+#
+# The same repeated number may be chosen from C unlimited number of
+# times.
+#
+# Note:
+#
+#     + All numbers (including target) will be positive integers.
+#     + Elements in a combination (a1, a2, ..., ak) must be in
+#       non-descending order. (ie, a1 <= a2 <= ... <= ak).
+#     + The solution set must not contain duplicate combinations.
+#
+# For example, given candidate set 2,3,6,7 and target 7, A solution set is:
+#
+#     [7]
+#     [2, 2, 3]
+
+
+# @param {Integer[]} candidates
+# @param {Integer} target
+# @return {Integer[][]}
+def combination_sum(candidates, target)
+  [].tap do |result|
+    _combination_sum_(candidates.sort, 0, candidates.size - 1, target, [], result)
+  end
+end
+
+private def _combination_sum_(nums, lbound, ubound, target, tracing, result)
+  sum = tracing.reduce(&:+) || 0
+  nums[lbound..ubound].each_with_index do |n, i|
+    break if sum + n > target
+
+    result << (tracing.dup << n) if sum + n == target
+    _combination_sum_(nums, lbound + i, ubound, target, tracing.dup << n, result)
+  end
+end
---
+++ algorithms/combination_sum_iii.rb
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/combination-sum-iii/
+#
+# Find all possible combinations of k numbers that add up to a
+# number n, given that only numbers from 1 to 9 can be used
+# and each combination should be a unique set of numbers.
+#
+# Ensure that numbers within the set are sorted in ascending order.
+#
+# Example 1:
+#
+#     Input: k = 3, n = 7
+#     Output: [[1,2,4]]
+#
+# Example 2:
+#
+#     Input: k = 3, n = 9
+#     Output: [[1,2,6], [1,3,5], [2,3,4]]
+
+
+# @param {Integer} k
+# @param {Integer} n
+# @return {Integer[][]}
+def combination_sum3(k, n)
+  [].tap do |result|
+    _combination_sum3_(1, 9, k, n, [], result)
+  end
+end
+
+private def _combination_sum3_(lbound, ubound, k, n, tracing, result)
+  sum = tracing.reduce(&:+) || 0
+  lbound.upto(ubound) do |i|
+    break if sum + i > n
+
+    result << (tracing.dup << i) if sum + i == n && tracing.size == k - 1
+    _combination_sum3_(i + 1, ubound, k, n, tracing.dup << i, result)
+  end
+end
