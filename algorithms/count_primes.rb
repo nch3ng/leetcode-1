67525bdd210bcd4c46c7d5751864531d292b1604
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:12:17 +0800

    solve: Count Primes

---
+++ algorithms/count_primes.rb
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/count-primes/
+#
+# Count the number of prime numbers less than a non-negative number, n.
+
+
+# @param {Integer} n
+# @return {Integer}
+def count_primes(n)
+  return 0 if n < 2
+
+  marks = Array.new(n, 1)
+  2.upto(Math.sqrt(n)) do |i|
+    next unless marks[i] == 1
+
+    j = i
+    marks[j] = 0 while (j += i) < n
+  end
+
+  marks.reduce(&:+) - 2
+end
