da1d5fbb9354bdb97378273f6099182be7c4414e
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 16:48:34 +0800

    solve: Binary Tree Paths

---
+++ algorithms/binary_tree_paths.rb
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/binary-tree-paths/
+#
+# Given a binary tree, return all root-to-leaf paths.
+#
+# For example, given the following binary tree:
+#
+#       1
+#      / \
+#     2   3
+#      \
+#       5
+#
+# All root-to-leaf paths are:
+#
+#     ["1->2->5", "1->3"]
+
+
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
+# end
+
+
+# @param {TreeNode} root
+# @return {String[]}
+def binary_tree_paths(root)
+  return [] if root.nil?
+  return [].tap { |paths| _traversal_(root, [], paths) }
+end
+
+private def _traversal_(root, tracing, paths)
+  tracing.push(root)
+
+  l, r = root.left, root.right
+  if l.nil? && r.nil?
+    paths << tracing.map(&:val).join('->')
+  else
+    _traversal_(l, tracing, paths) if l
+    _traversal_(r, tracing, paths) if r
+  end
+
+  tracing.pop
+end
