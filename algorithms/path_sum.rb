4439cab5c4d79b9b5937208b247a92da2271c27d
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:47:12 +0800

    solve: Path Sum I && II

---
+++ algorithms/path_sum.rb
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/path-sum/
+#
+# Given a binary tree and a sum, determine if the tree has a
+# root-to-leaf path such that adding up all the values along
+# the path equals the given sum.
+#
+# For example:
+#
+#     Given the below binary tree and sum = 22,
+#
+#               5
+#              / \
+#             4   8
+#            /   / \
+#           11  13  4
+#          / \       \
+#         7   2       1
+#
+#     return true, as there exist a root-to-leaf
+#     path 5->4->11->2 which sum is 22.
+
+
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
+# end
+
+
+# @param {TreeNode} root
+# @param {Integer} sum
+# @return {Boolean}
+def has_path_sum(root, sum)
+  return false if root.nil?
+  return _has_path_sum_(root, 0, sum)
+end
+
+private def _has_path_sum_(root, s, sum)
+  l, r, v = root.left, root.right, root.val
+  return s + v == sum if l.nil? && r.nil?
+
+  return true if l && _has_path_sum_(l, s + v, sum)
+  return true if r && _has_path_sum_(r, s + v, sum)
+  return false
+end
---
+++ algorithms/path_sum_ii.rb
@@ -0,0 +1,54 @@
+# https://leetcode.com/problems/path-sum-ii/
+#
+# Given a binary tree and a sum, find all root-to-leaf paths where
+# each path's sum equals the given sum.
+#
+# For example: Given the below binary tree and sum = 22,
+#
+#           5
+#          / \
+#         4   8
+#        /   / \
+#       11  13  4
+#      /  \    / \
+#     7    2  5   1
+#
+# return
+#
+#     [
+#       [5,4,11,2],
+#       [5,8,4,5]
+#     ]
+
+
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
+# end
+
+
+# @param {TreeNode} root
+# @param {Integer} sum
+# @return {Integer[][]}
+def path_sum(root, sum)
+  return [] if root.nil?
+  return [].tap { |paths| _traversal_(root, 0, sum, [], paths) }
+end
+
+private def _traversal_(root, s, sum, tracing, paths)
+  tracing.push(root)
+
+  l, r, v = root.left, root.right, root.val
+  if l.nil? && r.nil?
+    paths << tracing.map(&:val) if s + v == sum
+  else
+    _traversal_(l, s + v, sum, tracing, paths) if l
+    _traversal_(r, s + v, sum, tracing, paths) if r
+  end
+
+  tracing.pop
+end
