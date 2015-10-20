8812b3ee84c57f1941239e3cc6abd0835fe620af
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:50:15 +0800

    solve: Implement strStr()

---
+++ algorithms/implement_strstr.rb
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/implement-strstr/
+#
+# Implement strStr(). Returns the index of the first occurrence of
+# needle in haystack, or -1 if needle is not part of haystack.
+
+
+# @param {String} haystack
+# @param {String} needle
+# @return {Integer}
+def str_str(haystack, needle)
+  haystack.index(needle) || -1
+end
