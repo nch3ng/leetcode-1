3801fea147709bd5fb5ed034c7213f79a6f24558
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:31:53 +0800

    solve: Unique Paths I && II

---
+++ algorithms/unique_paths.rb
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/unique-paths/
+#
+# A robot is located at the top-left corner of a m x n grid (marked 'Start'
+# in the diagram below). The robot can only move either down or right at
+# any point in time. The robot is trying to reach the bottom-right corner
+# of the grid (marked 'Finish' in the diagram below). How many possible
+# unique paths are there?
+#
+#     +--------+--------+--------+--------+--------+--------+--------+
+#     | Start  |        |        |        |        |        |        |
+#     +--------+--------+--------+--------+--------+--------+--------+
+#     |        |        |        |        |        |        |        |
+#     +--------+--------+--------+--------+--------+--------+--------+
+#     |        |        |        |        |        |        | Finish |
+#     +--------+--------+--------+--------+--------+--------+--------+
+#
+#     Above is a 3 x 7 grid. How many possible unique paths are there?
+#
+# Note: m and n will be at most 100.
+
+
+# @param {Integer} m
+# @param {Integer} n
+# @return {Integer}
+def unique_paths(m, n)
+  grid = Array.new(m) { Array.new(n, 1) }
+
+  1.upto(m - 1) do |i|
+    1.upto(n - 1) do |j|
+      grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
+    end
+  end
+
+  grid[-1][-1]
+end
---
+++ algorithms/unique_paths_ii.rb
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/unique-paths-ii/
+#
+# Follow up for "Unique Paths": Now consider if some obstacles are added
+# to the grids. How many unique paths would there be? An obstacle and
+# empty space is marked as 1 and 0 respectively in the grid.
+#
+# For example, There is one obstacle in the middle of a 3x3 grid as
+# illustrated below.
+#
+#     [
+#       [0,0,0],
+#       [0,1,0],
+#       [0,0,0]
+#     ]
+#
+# The total number of unique paths is 2.
+#
+# Note: m and n will be at most 100.
+
+
+# @param {Integer[][]} obstacle_grid
+# @return {Integer}
+def unique_paths_with_obstacles(obstacle_grid)
+  blocked = ->(i, j) { obstacle_grid[i][j] == 1 }
+
+  m, n = obstacle_grid.size, obstacle_grid[0].size
+  grid = Array.new(m) { Array.new(n) }
+
+  grid[0][0] = blocked[0, 0] ? 0 : 1
+  1.upto(m - 1) { |i| grid[i][0] = blocked[i - 1, 0] ? 0 : grid[i - 1][0] }
+  1.upto(n - 1) { |j| grid[0][j] = blocked[0, j - 1] ? 0 : grid[0][j - 1] }
+
+  1.upto(m - 1) do |i|
+    1.upto(n - 1) do |j|
+      grid[i][j]  = 0
+      grid[i][j] += blocked[i - 1, j] ? 0 : grid[i - 1][j]
+      grid[i][j] += blocked[i, j - 1] ? 0 : grid[i][j - 1]
+    end
+  end
+
+  blocked[m - 1, n - 1] ? 0 : grid[m - 1][n - 1]
+end
