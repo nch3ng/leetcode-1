89076f8c83e78a6ceaf957e34612469b63b173b4
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:23:14 +0800

    solve: Find Minimum in Rotated Sorted Array I && II

---
+++ algorithms/find_minimum_in_rotated_sorted_array.rb
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+#
+# Suppose a sorted array is rotated at some pivot unknown to you
+# beforehand.
+#
+#     (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+#
+# Find the minimum element. You may assume no duplicate exists in
+# the array.
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def find_min(nums)
+  return nums[0] if nums[0] < nums[-1]
+
+  lbound, ubound = 0, nums.size - 1
+  while ubound - lbound > 1
+    m = (ubound + lbound) / 2
+
+    if nums[m] < nums[lbound]
+      ubound = m
+    else
+      lbound = m
+    end
+  end
+
+  nums[ubound]
+end
---
+++ algorithms/find_minimum_in_rotated_sorted_array_ii.rb
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+#
+# Follow up for "Find Minimum in Rotated Sorted Array":
+#
+#     What if duplicates are allowed?
+#     Would this affect the run-time complexity? How and why?
+#
+# Suppose a sorted array is rotated at some pivot unknown to you
+# beforehand.
+#
+#     (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+#
+# Find the minimum element. The array may contain duplicates.
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def find_min(nums)
+  return nums[0] if nums[0] < nums[-1]
+
+  lbound, ubound = 0, nums.size - 1
+  while ubound - lbound > 1
+    if nums[lbound] == nums[ubound]
+      lbound += 1 while lbound < ubound && nums[lbound] == nums[ubound]
+      return nums[lbound] if nums[lbound] < nums[ubound]
+    end
+
+    m = (ubound + lbound) / 2
+
+    if nums[m] < nums[lbound]
+      ubound = m
+    else
+      lbound = m
+    end
+  end
+
+  nums[ubound]
+end
