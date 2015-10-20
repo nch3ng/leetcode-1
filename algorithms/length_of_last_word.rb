819dc13fa292c7b457fd7f6f17d35f9dac901c24
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:14:12 +0800

    solve: Length of Last Word

---
+++ algorithms/length_of_last_word.rb
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/length-of-last-word/
+#
+# Given a string s consists of upper/lower-case alphabets and empty
+# space characters ' ', return the length of last word in the
+# string. If the last word does not exist, return 0.
+#
+# Note: A word is defined as a character sequence consists of non-space
+# characters only. For example,
+#
+#     Given s = "Hello World", return 5.
+
+
+# @param {String} s
+# @return {Integer}
+def length_of_last_word(s)
+  str = s.reverse.lstrip
+  str.index(' ') || str.size
+end
