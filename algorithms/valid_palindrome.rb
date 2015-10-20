928363368518e9a9bb08f78246f7c6df4f3442ed
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:33:56 +0800

    solve: Valid Palindrome

---
+++ algorithms/valid_palindrome.rb
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/valid-palindrome/
+#
+# Given a string, determine if it is a palindrome, considering only
+# alphanumeric characters and ignoring cases.
+#
+# For example,
+#
+#     "A man, a plan, a canal: Panama" is a palindrome.
+#     "race a car" is not a palindrome.
+
+
+# @param {String} s
+# @return {Boolean}
+def is_palindrome(s)
+  i1, i2 = 0, s.size - 1
+
+  while true
+    cmp1 =
+      while true
+        break if i1 > i2
+        c1 = s[i1]; i1 += 1
+        break true if c1 =~ /\p{Alnum}/
+      end
+
+    cmp2 =
+      while true
+        break if i1 > i2
+        c2 = s[i2]; i2 -= 1
+        break true if c2 =~ /\p{Alnum}/
+      end
+
+    return true unless cmp1 && cmp2
+    return false if c1.downcase != c2.downcase
+  end
+end
