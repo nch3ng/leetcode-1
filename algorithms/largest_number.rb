a19efad3224ed65e0e213b399596c19903accef1
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:13:06 +0800

    solve: Largest Number

---
+++ algorithms/largest_number.rb
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/largest-number/
+#
+# Given a list of non negative integers, arrange them such that
+# they form the largest number.
+#
+# For example, given [3, 30, 34, 5, 9], the largest formed number
+# is 9534330.
+#
+# Note: The result may be very large, so you need to return a
+# string instead of an integer.
+
+
+# @param {Integer[]} nums
+# @return {String}
+def largest_number(nums)
+  f = ->(x1, x2) { x2 + x1 <=> x1 + x2 }
+  n = nums.map(&:to_s).sort(&f).reduce(&:+)
+
+  n[(n.index(/[^0]/) || -1)..-1]
+end
