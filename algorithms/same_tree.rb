63f941738916f282a0942956973d6a33fd6e4e54
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:16:57 +0800

    solve: Same Tree

---
+++ algorithms/same_tree.rb
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/same-tree/
+#
+# Given two binary trees, write a function to check if they are equal
+# or not. Two binary trees are considered equal if they are structurally
+# identical and the nodes have the same value.
+
+
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
+# end
+
+
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {Boolean}
+def is_same_tree(p, q)
+  return true if p.nil? && q.nil?
+  return false if p.nil? || q.nil? || p.val != q.val
+  return is_same_tree(p.left, q.left) && is_same_tree(p.right, q.right)
+end
