df26fe0b33b770191e469a2a75b28240a650c54c
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:41:07 +0800

    solve: Palindrome Partitioning I && II

---
+++ algorithms/palindrome_partitioning.rb
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/palindrome-partitioning/
+#
+# Given a string s, partition s such that every substring of the
+# partition is a palindrome.
+#
+# Return all possible palindrome partitioning of s.
+#
+# For example, given s = "aab", Return
+#
+#     [
+#       ["aa","b"],
+#       ["a","a","b"]
+#     ]
+
+
+# @param {String} s
+# @return {String[][]}
+def partition(s)
+  [].tap { |result| _partition_(s, 0, [], result) }
+end
+
+private def _partition_(s, spt, tracing, result)
+  if spt == s.size
+    result << tracing; return
+  end
+
+  spt.upto(s.size - 1) do |ubound|
+    l, u = spt, ubound
+    while l < u && s[l] == s[u]
+      l += 1; u -= 1
+    end
+    next if s[l] != s[u]
+
+    _partition_(s, ubound + 1, tracing.dup << s[spt..ubound], result)
+  end
+end
---
+++ algorithms/palindrome_partitioning_ii.rb
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/palindrome-partitioning-ii/
+#
+# Given a string s, partition s such that every substring of the
+# partition is a palindrome. Return the minimum cuts needed for
+# a palindrome partitioning of s.
+#
+# For example, given s = "aab", Return 1 since the palindrome
+# partitioning ["aa","b"] could be produced using 1 cut.
+
+
+# @param {String} s
+# @return {Integer}
+def min_cut(s)
+  return 0 if _is_palindrome_(s, 0, s.size - 1)
+
+  min, q, marks = 1, [0, nil], Array.new(s.size)
+  while true
+    lbound = q.shift
+
+    if lbound.nil?
+      min += 1
+      q << nil; next
+    end
+
+    Range.new(lbound, s.size - 2).each do |spt|
+      next unless _is_palindrome_(s, lbound, spt)
+
+      nextlbound = spt + 1
+      return min if _is_palindrome_(s, nextlbound, s.size - 1)
+
+      q << nextlbound unless marks[nextlbound]
+      marks[nextlbound] = true
+    end
+  end
+end
+
+private def _is_palindrome_(s, lbound, ubound)
+  while lbound < ubound && s[lbound] == s[ubound]
+    lbound += 1; ubound -= 1
+  end
+  s[lbound] == s[ubound]
+end
