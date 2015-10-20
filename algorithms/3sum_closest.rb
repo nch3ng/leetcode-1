73f6646055a5c4e2ef9a4a5f40a1f7b3d1f39ce7
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 16:08:58 +0800

    solve: 3Sum Closest

---
+++ algorithms/3sum_closest.rb
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/3sum-closest/
+#
+# Given an array S of n integers, find three integers in S such
+# that the sum is closest to a given number, target. Return the
+# sum of the three integers. You may assume that each input
+# would have exactly one solution.
+#
+# For example, given array S = {-1 2 1 -4}, and target = 1. The
+# sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def three_sum_closest(nums, target)
+  nums = nums.sort
+
+  distance = nums[0..2].reduce(&:+) - target
+  0.upto(nums.size - 3) do |ai|
+    bi, ci = ai + 1, nums.size - 1
+
+    while bi < ci
+      sum = nums[ai] + nums[bi] + nums[ci]
+
+      dis = sum - target
+      return sum if dis == 0
+
+      distance = dis if distance.abs > dis.abs
+      bi += 1 if dis < 0
+      ci -= 1 if dis > 0
+    end
+  end
+
+  target + distance
+end
