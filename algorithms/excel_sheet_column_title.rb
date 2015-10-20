659a8d00106292bd9ab44be2ffbe5eaaa5d725ba
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:19:36 +0800

    solve: Excel Sheet Column Title

---
+++ algorithms/excel_sheet_column_title.rb
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/excel-sheet-column-title/
+#
+# Given a positive integer, return its corresponding column title
+# as appear in an Excel sheet.
+#
+# For example:
+#
+#     A -> 1
+#     B -> 2
+#     C -> 3
+#     ...
+#     Z -> 26
+#     AA -> 27
+#     AB -> 28
+
+
+# @param {Integer} n
+# @return {String}
+def convert_to_title(n)
+  m = {}
+  Range.new('A', 'Z').each_with_index { |ch, idx| m[idx] = ch }
+
+  str = ''
+  while n > 0
+    str.insert(0, m[(n - 1) % 26])
+    n = (n - 1) / 26
+  end
+  str
+end
