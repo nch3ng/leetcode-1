f9e249a97c1de8874a40b97413434e4495305610
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:19:06 +0800

    solve: Simplify Path

---
+++ algorithms/simplify_path.rb
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/simplify-path/
+#
+# Given an absolute path for a file (Unix-style), simplify it.
+#
+# For example,
+#
+#     path = "/home/", => "/home"
+#     path = "/a/./b/../../c/", => "/c"
+
+
+# @param {String} path
+# @return {String}
+def simplify_path(path)
+  s = []
+
+  path.split('/').each do |e|
+    case e
+    when ''   ; nil
+    when '.'  ; nil
+    when '..' ; s.pop
+    else      ; s.push(e)
+    end
+  end
+
+  "/#{s.join('/')}"
+end
