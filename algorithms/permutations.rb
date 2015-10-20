c26fe3e162605fa84ae3cf06fcd35f0cb45d29e8
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:50:23 +0800

    solve: Permutations I && II

---
+++ algorithms/permutations.rb
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/permutations/
+#
+# Given a collection of numbers, return all possible permutations.
+#
+# For example,
+#
+#     [1,2,3] have the following permutations:
+#     [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1]
+
+
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute(nums)
+  nums.permutation.to_a
+end
---
+++ algorithms/permutations_ii.rb
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/permutations-ii/
+#
+# Given a collection of numbers that might contain duplicates, return
+# all possible unique permutations.
+#
+# For example,
+#
+#     [1,1,2] have the following unique permutations:
+#     [1,1,2], [1,2,1], and [2,1,1].
+
+
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute_unique(nums)
+  [].tap do |result|
+    _permute_unique_(nums, 0, [], result)
+  end
+end
+
+private def _permute_unique_(nums, idx, tracing, result)
+  if idx == nums.size
+    result << tracing; return
+  end
+
+  m = {}
+  idx.upto(nums.size - 1) do |i|
+    next if m[nums[i]]
+    m[nums[i]] = true
+
+    nums[i], nums[idx] = nums[idx], nums[i]
+    _permute_unique_(nums, idx + 1, tracing.dup << nums[idx], result)
+    nums[i], nums[idx] = nums[idx], nums[i]
+  end
+end
