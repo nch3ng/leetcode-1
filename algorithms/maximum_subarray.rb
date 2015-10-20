36c9f4ffdfc5013bcfb64fe53fc26bb51672c21a
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:33:00 +0800

    solve: Maximum Subarray

---
+++ algorithms/maximum_subarray.rb
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/maximum-subarray/
+#
+# Find the contiguous subarray within an array (containing at least
+# one number) which has the largest sum.  For example, given the
+# array [−2,1,−3,4,−1,2,1,−5,4], the contiguous subarray [4,−1,2,1]
+# has the largest sum = 6.
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def max_sub_array(nums)
+  sum, subsum = nums[0], nums[0]
+
+  1.upto(nums.size - 1) do |idx|
+    subsum  = 0 if subsum < 0
+    subsum += nums[idx]
+
+    sum = subsum if sum < subsum
+  end
+  sum
+end
