f0c6cf9d65bc14481b51fe7ab9942b8e7c82a790
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:54:59 +0800

    solve: Isomorphic Strings

---
+++ algorithms/isomorphic_strings.rb
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/isomorphic-strings/
+#
+# Given two strings s and t, determine if they are isomorphic. Two
+# strings are isomorphic if the characters in s can be replaced to
+# get t. All occurrences of a character must be replaced with another
+# character while preserving the order of characters. No two characters
+# may map to the same character but a character may map to itself.
+#
+# For example,
+#
+#     Given "egg", "add", return true.
+#     Given "foo", "bar", return false.
+#     Given "paper", "title", return true.
+#
+# Note: You may assume both s and t have the same length.
+
+
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_isomorphic(s, t)
+  m1, m2 = {}, {}
+
+  s.chars.each_with_index do |ch, idx|
+    tc = t[idx]
+
+    if m1.key?(ch) || m2.key?(tc)
+      return false if m1[ch] != tc
+      return false if m2[tc] != ch
+    else
+      m1[ch] = tc
+      m2[tc] = ch
+    end
+  end
+  return true
+end
