85a676d694b68f1142bcf749557d3ece1eb1eee8
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:46:26 +0800

    solve: Group Anagrams

---
+++ algorithms/anagrams.rb
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/anagrams/
+#
+# Given an array of strings, group anagrams together.
+#
+# For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], Return:
+#
+#     [
+#       ["ate", "eat", "tea"],
+#       ["nat", "tan"],
+#       ["bat"]
+#     ]
+#
+# Note:
+#
+#     + For the return value, each inner list's elements must follow
+#       the lexicographic order.
+#     + All inputs will be in lower-case.
+
+
+# @param {String[]} strs
+# @return {String[][]}
+def group_anagrams(strs)
+  strs.group_by(&->(s){ s.chars.sort.join }).values.map(&:sort)
+end
