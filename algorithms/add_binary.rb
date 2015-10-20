2d1e28f90b6a106ee318a5560e5a90cd7f632bdb
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 16:11:36 +0800

    solve: Add Binary

---
+++ algorithms/add_binary.rb
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/add-binary/
+#
+# Given two binary strings, return their sum (also a binary string).
+#
+# For example,
+#
+#     a = "11"
+#     b = "1"
+#     Return "100"
+
+
+# @param {String} a
+# @param {String} b
+# @return {String}
+def add_binary(a, b)
+  (a.to_i(2) + b.to_i(2)).to_s(2)
+end
