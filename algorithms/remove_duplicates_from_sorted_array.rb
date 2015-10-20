20791a9ba61414ef3b8c0b948864a9c50b42edec
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:54:22 +0800

    solve: Remove Duplicates from Sorted Array I && II

---
+++ algorithms/remove_duplicates_from_sorted_array.rb
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+#
+# Given a sorted array, remove the duplicates in place such that each
+# element appear only once and return the new length. Do not allocate
+# extra space for another array, you must do this in place with constant
+# memory.
+#
+# For example,
+#
+#     Given input array nums = [1,1,2],
+#     Your function should return length = 2, with the first two
+#     elements of nums being 1 and 2 respectively. It doesn't matter
+#     what you leave beyond the new length.
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def remove_duplicates(nums)
+  size, cursor = 0, 0
+
+  while cursor < nums.size
+    num = nums[cursor]
+    cursor += 1
+    while nums[cursor] == num
+      cursor += 1
+    end
+
+    nums[size] = num; size += 1
+  end
+
+  size
+end
---
+++ algorithms/remove_duplicates_from_sorted_array_ii.rb
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+#
+# Follow up for "Remove Duplicates":
+#
+#     What if duplicates are allowed at most twice?
+#
+# For example, Given sorted array nums = [1,1,1,2,2,3], Your function
+# should return length = 5, with the first five elements of nums
+# being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the
+# new length.
+
+
+# @param {Integer[]} nums
+# @return {Integer}
+def remove_duplicates(nums)
+  size, cursor = 0, 0
+
+  while cursor < nums.size
+    num, dup = nums[cursor], false
+    cursor += 1
+    while nums[cursor] == num
+      cursor += 1
+      dup = true
+    end
+
+    nums[size] = num; size += 1
+    next unless dup
+
+    nums[size] = num; size += 1
+  end
+
+  size
+end
