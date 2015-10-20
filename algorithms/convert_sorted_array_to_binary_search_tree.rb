0bb4e187cbbe51b587d12ef4d5c18a975b7bfe77
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 17:09:58 +0800

    solve: Convert Sorted Array to Binary Search Tree

---
+++ algorithms/convert_sorted_array_to_binary_search_tree.rb
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+#
+# Given an array where elements are sorted in ascending order, convert
+# it to a height balanced BST.
+
+
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
+# end
+
+
+# @param {Integer[]} nums
+# @return {TreeNode}
+def sorted_array_to_bst(nums)
+  _sorted_array_to_bst_(nums, 0, nums.size)
+end
+
+private def _sorted_array_to_bst_(nums, lbound, ubound)
+  return nil unless lbound < ubound
+
+  m = (lbound + ubound) / 2
+
+  l = _sorted_array_to_bst_(nums, lbound, m)
+  r = _sorted_array_to_bst_(nums, m + 1, ubound)
+  return TreeNode.new(nums[m]).tap { |n| n.left, n.right = l, r }
+end
