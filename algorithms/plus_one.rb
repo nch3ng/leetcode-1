ef659e713f81dc051e0d4aac62cb137bca505f6b
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:49:15 +0800

    solve: Plus One

---
+++ algorithms/plus_one.rb
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/plus-one/
+#
+# Given a non-negative number represented as an array of digits, plus
+# one to the number. The digits are stored such that the most significant
+# digit is at the head of the list.
+
+
+# @param {Integer[]} digits
+# @return {Integer[]}
+def plus_one(digits)
+  sum, carry = [], 1
+
+  digits.reverse_each do |digit|
+    if carry.nonzero?
+      value = digit + carry
+      carry, value = value.divmod(10)
+    else
+      value = digit
+    end
+
+    sum.insert(0, value)
+  end
+  sum.insert(0, carry) if carry.nonzero?
+
+  sum
+end
