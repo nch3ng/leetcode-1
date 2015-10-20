84386ca85af2beb96db2d6b5315b94ca41be5add
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:39:06 +0800

    solve: Minimum Size Subarray Sum

---
+++ algorithms/minimum_size_subarray_sum.rb
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/minimum-size-subarray-sum/
+#
+# Given an array of n positive integers and a positive integer s,
+# find the minimal length of a subarray of which the sum ≥ s. If
+# there isn't one, return 0 instead.
+#
+# For example, given the array [2,3,1,2,4,3] and s = 7, the subarray
+# [4,3] has the minimal length under the problem constraint.
+
+
+# @param {Integer} s
+# @param {Integer[]} nums
+# @return {Integer}
+def min_sub_array_len(s, nums)
+  sum = 0
+  idx = nums.find_index { |num| (sum += num) >= s }
+  return 0 unless idx
+
+  minlen = idx + 1
+  while true
+    while sum >= s && sum - nums[idx - minlen + 1] >= s
+      sum -= nums[idx - minlen + 1]
+      minlen -= 1
+    end
+
+    idx += 1
+    break if idx >= nums.size
+    sum = sum + nums[idx] - nums[idx - minlen]
+  end
+  minlen
+end
