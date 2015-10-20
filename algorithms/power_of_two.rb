4fc5c278e0771ad04034620de70b089388276a9b
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:51:39 +0800

    solve: Power of Two

---
+++ algorithms/power_of_two.rb
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/power-of-two/
+#
+# Given an integer, write a function to determine if it is a power of two.
+
+
+# @param {Integer} n
+# @return {Boolean}
+def is_power_of_two(n)
+  return false if n <= 0
+
+  while n > 1
+    return false if n % 2 != 0
+    n = n / 2
+  end
+  return true
+end
