bba6c91f48470d0f698b70a93fb14503459d03b3
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:41:46 +0800

    solve: Multiply Strings

---
+++ algorithms/multiply_strings.rb
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/multiply-strings/
+#
+# Given two numbers represented as strings, return multiplication
+# of the numbers as a string.
+#
+# Note: The numbers can be arbitrarily large and are non-negative.
+
+
+# @param {String} num1
+# @param {String} num2
+# @return {String}
+def multiply(num1, num2)
+  (num1.to_i * num2.to_i).to_s
+end
