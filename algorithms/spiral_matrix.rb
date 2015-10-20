b557fb9997f4fbc9fc81b0d4fcff900fafc175c4
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:20:57 +0800

    solve: Spiral Matrix I && II

---
+++ algorithms/spiral_matrix.rb
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/spiral-matrix/
+#
+# Given a matrix of m x n elements (m rows, n columns), return all
+# elements of the matrix in spiral order.
+#
+# For example, Given the following matrix:
+#
+#     [
+#       [ 1, 2, 3 ],
+#       [ 4, 5, 6 ],
+#       [ 7, 8, 9 ]
+#     ]
+#
+# You should return [1,2,3,6,9,8,7,4,5].
+
+
+# @param {Integer[][]} matrix
+# @return {Integer[]}
+def spiral_order(matrix)
+  return [] if matrix.empty?
+
+  m, n, s = matrix.size, matrix[0].size, []
+  x, y, u, v = -1, -1, m, n
+  while true
+    x, y, u, v = x + 1, y + 1, u - 1, v - 1
+    rs, cs =  u - x + 1, v - y + 1
+
+    y.upto(v) { |j| s << matrix[x][j] }
+    return s if rs <= 1
+
+    (x + 1).upto(u) { |i| s << matrix[i][v] }
+    (v - 1).downto(y) { |j| s << matrix[u][j] }
+    (u - 1).downto(x + 1) { |i| s << matrix[i][y] } if cs > 1
+    return s if s.size == m * n
+  end
+end
---
+++ algorithms/spiral_matrix_ii.rb
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/spiral-matrix-ii/
+#
+# Given an integer n, generate a square matrix filled with elements
+# from 1 to n^2 in spiral order.
+#
+# For example, Given n = 3, You should return the following matrix:
+#
+#     [
+#       [ 1, 2, 3 ],
+#       [ 8, 9, 4 ],
+#       [ 7, 6, 5 ]
+#     ]
+
+
+# @param {Integer} n
+# @return {Integer[][]}
+def generate_matrix(n)
+  return [] if n.zero?
+
+  matrix, count = Array.new(n, &Proc.new{ Array.new(n) }), 0
+  x, y, u, v = -1, -1, n, n
+  while true
+    x, y, u, v = x + 1, y + 1, u - 1, v - 1
+    rs, cs =  u - x + 1, v - y + 1
+
+    y.upto(v) { |j| matrix[x][j] = (count += 1); }
+    return matrix if rs <= 1
+
+    (x + 1).upto(u) { |i| matrix[i][v] = (count += 1) }
+    (v - 1).downto(y) { |j| matrix[u][j] = (count += 1) }
+    (u - 1).downto(x + 1) { |i| matrix[i][y] = (count += 1) } if cs > 1
+    return matrix if count == n * n
+  end
+end
