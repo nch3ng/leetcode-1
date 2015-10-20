167c659ac745fc35960533539fbaa52621fc08b9
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 18:27:01 +0800

    solve: Longest Valid Parentheses

---
+++ algorithms/longest_valid_parentheses.rb
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/longest-valid-parentheses/
+#
+# Given a string containing just the characters '(' and ')', find the
+# length of the longest valid (well-formed) parentheses substring.
+#
+# For "(()", the longest valid parentheses substring is "()", which
+# has length = 2. Another example is ")()())", where the longest valid
+# parentheses substring is "()()", which has length = 4.
+
+
+# @param {String} s
+# @return {Integer}
+def longest_valid_parentheses(s)
+  maxlen, stack = 0, []
+
+  bound = s.index('(') || s.size
+  stack.push(bound - 1)
+
+  bound.upto(s.size - 1) do |idx|
+    if s[idx] == '('
+      stack.push(idx); next
+    end
+
+    stack.pop
+    if stack.empty?
+      stack.push(idx)
+    else
+      maxlen = [maxlen, idx - stack[-1]].max
+    end
+  end
+
+  maxlen
+end
