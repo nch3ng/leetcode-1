f8624df7698eb34131880f33d77e502304e0103f
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:10:27 +0800

    solve: Search for a Range

---
+++ algorithms/search_for_a_range.rb
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/search-for-a-range/
+#
+# Given a sorted array of integers, find the starting and ending position
+# of a given target value. If the target is not found in the array,
+# return [-1, -1]. For example
+#
+#     Given [5, 7, 7, 8, 8, 10] and target value 8, return [3, 4].
+#
+# Your algorithm's runtime complexity must be in the order of O(logn).
+
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def search_range(nums, target)
+  lbound = (0...nums.size).bsearch { |i| nums[i] >= target }
+  return [-1, -1] if lbound.nil?
+  return [-1, -1] if nums[lbound] != target
+
+  ubound = (lbound...nums.size).bsearch { |i| nums[i] > target }
+  return [lbound, nums.size - 1] if ubound.nil?
+  return [lbound, ubound - 1]
+end
