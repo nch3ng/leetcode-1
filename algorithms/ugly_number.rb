91002edc133f98f46f3d36954614c769156f8769
0x01f7 <souk.0x01f7@gmail.com>
2015-10-20 19:30:12 +0800

    solve: Ugly Number I && II

---
+++ algorithms/ugly_number.rb
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/ugly-number/
+#
+# Write a program to check whether a given number is an ugly number.
+# Ugly numbers are positive numbers whose prime factors only include
+# 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it
+# includes another prime factor 7.
+
+
+# @param {Integer} num
+# @return {Boolean}
+def is_ugly(num)
+  return false if num <= 0
+
+  num = num / 5 while num % 5 == 0
+  num = num / 3 while num % 3 == 0
+  num = num / 2 while num % 2 == 0
+  num == 1
+end
---
+++ algorithms/ugly_number_ii.rb
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/ugly-number-ii/
+#
+# Write a program to find the n-th ugly number. Ugly numbers are
+# positive numbers whose prime factors only include 2, 3, 5. For
+# example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the
+# first 10 ugly numbers. Note that 1 is typically treated as an
+# ugly number.
+
+
+# @param {Integer} n
+# @return {Integer}
+def nth_ugly_number(n)
+  nums, i2, i3, i5 = Array.new(n, 1), 0, 0, 0
+
+  1.upto(n - 1) do |i|
+    n2, n3, n5 = nums[i2] * 2, nums[i3] * 3, nums[i5] * 5
+
+    min = n2  < n3 ? n2  : n3
+    min = min < n5 ? min : n5
+    nums[i] = min
+
+    i2 += 1 if min == n2
+    i3 += 1 if min == n3
+    i5 += 1 if min == n5
+  end
+
+  nums[n - 1]
+end
