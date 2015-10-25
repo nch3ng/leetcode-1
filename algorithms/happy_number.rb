65bb25a012043aa153a430b9cc007044f2f3b1ce
0x01f7 <souk.0x01f7@gmail.com>
2015-10-26 00:55:11 +0800

    style: comment

--- algorithms/happy_number.rb
+++ algorithms/happy_number.rb
@@ -1,11 +1,11 @@
 # https://leetcode.com/problems/happy-number/
 #
-# Write an algorithm to determine if a number is "happy". A happy number
-# is a number defined by the following process: Starting with any
-# positive integer, replace the number by the sum of the squares of its
-# digits, and repeat the process until the number equals 1 (where it
-# will stay), or it loops endlessly in a cycle which does not include 1.
-# Those numbers for which this process ends in 1 are happy numbers.
+# Write an algorithm to determine if a number is "happy". A happy number is a
+# number defined by the following process: Starting with any positive integer,
+# replace the number by the sum of the squares of its digits, and repeat the
+# process until the number equals 1 (where it will stay), or it loops
+# endlessly in a cycle which does not include 1. Those numbers for which this
+# process ends in 1 are happy numbers.
 #
 # Example: 19 is a happy number
 #
@@ -13,6 +13,11 @@
 #     8^2 + 2^2 = 68
 #     6^2 + 8^2 = 100
 #     1^2 + 0^2 + 0^2 = 1
+#
+# Credits:
+#
+#     Special thanks to @mithmatt and @ts for adding this problem and creating
+#     all test cases.
 
 
 # @param {Integer} n
