a9c55339e3c4cb8ca46e1365d78ef6c21be994f1
0x01f7 <souk.0x01f7@gmail.com>
2015-10-26 01:04:26 +0800

    style: comment

--- algorithms/jump_game.rb
+++ algorithms/jump_game.rb
@@ -1,9 +1,9 @@
 # https://leetcode.com/problems/jump-game/
 #
-# Given an array of non-negative integers, you are initially positioned
-# at the first index of the array. Each element in the array represents
-# your maximum jump length at that position. Determine if you are able
-# to reach the last index.
+# Given an array of non-negative integers, you are initially positioned at the
+# first index of the array. Each element in the array represents your maximum
+# jump length at that position. Determine if you are able to reach the last
+# index.
 #
 # For example:
 #
