68a7892edd9374a246168042f9a29a849e1cd675
0x01f7 <souk.0x01f7@gmail.com>
2015-11-06 00:30:53 +0800

    optimize: Remove Invalid Parentheses

--- algorithms/remove_invalid_parentheses.rb
+++ algorithms/remove_invalid_parentheses.rb
@@ -22,33 +22,28 @@
 # @param {String} s
 # @return {String[]}
 def remove_invalid_parentheses(s)
-  return [''] if s.empty?
+  q = [['', 0]]
+  s.each_char do |ch|
+    q.uniq!; q << nil
 
-  q, idx = [['', 0], nil], 0
-  while true
-    e = q.shift
-
-    if e.nil?
-      break if (idx += 1) == s.size
-      q = q.uniq << nil; next
-    end
-
-    substr, lparen = e
-    case s[idx]
-    when '('; q << e; q << [substr + '(', lparen + 1]
-    when ')'; q << e; q << [substr + ')', lparen - 1] if lparen > 0
-    else    ; q << [substr + s[idx], lparen]
+    while (e = q.shift)
+      substr, lparen = e
+      case ch
+      when '('; q << e; q << [substr + '(', lparen + 1]
+      when ')'; q << e; q << [substr + ')', lparen - 1] if lparen > 0
+      else    ; q << [substr + ch, lparen]
+      end
     end
   end
 
   out, maxlen = [], 0
-  q.each do |qe|
-    next if qe[1].nonzero?
+  q.each do |e|
+    next if e[1].nonzero?
 
-    case qe[0].size <=> maxlen
+    case e[0].size <=> maxlen
     when -1; next
-    when  0; out << qe[0]
-    when  1; out.clear; out << qe[0]; maxlen = qe[0].size
+    when  0; out << e[0]
+    when  1; out.clear; out << e[0]; maxlen = e[0].size
     end
   end
   out.uniq
